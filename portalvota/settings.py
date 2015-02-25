7a9ddb86c6c150f8c7fadc874417cf93f32a31c7
edsonlb <ti_edson@yahoo.com.br>
2015-02-25 15:02:08 -0300

    Arrumando Banco Postgree e banco local

--- portalvota/settings.py
+++ portalvota/settings.py
@@ -52,14 +52,10 @@ WSGI_APPLICATION = 'portalvota.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-'''
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
 }
-'''
 
 DATABASES['default'] =  dj_database_url.config()
 
