044932f6a19aeca7a6d8838eb745931357248c6d
edsonlb <ti_edson@yahoo.com.br>
2015-02-25 16:21:26 -0300

    Verificando banco remoto

--- portalvota/settings.py
+++ portalvota/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'r&v-q$@y(6tot1=7j#uq3+r9$qg6$3bdloya61y8iazvy0a#zg'
 
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,12 +52,16 @@ WSGI_APPLICATION = 'portalvota.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
-}
+if 'PRODUCTION' in os.environ:
+    DATABASES = { 'default': dj_database_url.config() }
+else:
+    DATABASES = { 'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}
+    }
 
-DATABASES['default'] =  dj_database_url.config()
+
+#DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
