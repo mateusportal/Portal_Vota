1b86c8cb3d62d3e29135ef86ea223c3c6a75771c
edsonlb <ti_edson@yahoo.com.br>
2015-03-04 15:28:03 -0300

    Arrumando os melhores do ano

--- core/views.py
+++ core/views.py
@@ -19,11 +19,15 @@ def vencedores(request):
 
     datas = Voto.objects.all().datetimes('data_cadastro','month',order='DESC')
     listaVotos = {}
-    #for dt in datas:
-        #votos = Voto.objects.filter(data_cadastro__month=dt.month, data_cadastro__year=dt.year).values('destinatario_id').annotate(dcount=Count('id')).order_by('-dcount')[:1]
-        #listaVotos = itertools.chain(listaVotos,votos,['1':2])
+    for dt in datas:
+        votos = Voto.objects.filter(data_cadastro__month=dt.month, data_cadastro__year=dt.year).values('destinatario_id').annotate(dcount=Count('id')).order_by('-dcount')[:1]
+
+        listaVotos = itertools.chain(listaVotos,votos.destinatario_id)
 
     print list(listaVotos)
 
-    return render(request,'vencedores.html',{'votos':listaVotos})
+    #tenho que retornar algo assim para o template para mostrar e listar os ganhadores por ano e mes, e claro, a foto com o nome
+    listagem = [{'ano': 2015, 'mes': 3, 'nome': 'edson', 'foto': 'leo.jpg'}, {'ano': 2014, 'mes': 2, 'nome': 'Leo', 'foto': 'claudio.jpg'}]
+
+    return render(request,'vencedores.html',{'listagem':listagem})
 
