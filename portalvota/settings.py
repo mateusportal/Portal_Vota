d15e24e7b070bcc2cd5c2d0c5d8353318f204347
edsonlb <ti_edson@yahoo.com.br>
2015-03-06 16:35:23 -0300

    Melhorando a seguranca do Debug

--- portalvota/settings.py
+++ portalvota/settings.py
@@ -7,19 +7,10 @@ import os
 import dj_database_url
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = CHAVE
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -60,8 +51,12 @@ WSGI_APPLICATION = 'portalvota.wsgi.application'
 
 
 if 'PRODUCTION' in os.environ:
+    DEBUG = False
+    TEMPLATE_DEBUG = False
     DATABASES = { 'default': dj_database_url.config() }
 else:
+    DEBUG = True
+    TEMPLATE_DEBUG = True
     DATABASES = { 'default': {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}
